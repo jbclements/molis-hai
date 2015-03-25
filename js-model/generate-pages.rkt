3fbb204d07fa8838675dbe0da734a60d7ea7d41f
John Clements <clements@racket-lang.org>
2015-03-25 16:47:36 -0700

    working on auto-page-generation for multiple models

---
+++ js-model/generate-pages.rkt
@@ -0,0 +1,27 @@
+#lang racket
+
+(require web-server/templates
+         racket/runtime-path
+         xml)
+
+(define-runtime-path here ".")
+
+;; plug vars into the index template
+(define (make-pwgen-page order tree-hash-path seed-tree-path)
+  (include-template "pwgen-template.html"))
+
+;; add quotes
+(define (with-quotes str)
+  (~a "\""str"\""))
+
+(define (build-page order)
+  (display-to-file
+   (make-pwgen-page order
+                    (with-quotes (~a "atotc-"order"-tree-hash.js"))
+                    (with-quotes (~a "atotc-"order"-seed-tree.js")))
+   (build-path here (~a "pwgen-"order".html"))
+   #:exists 'truncate))
+
+(for ([i (in-range 1 4)])
+  (build-page i))
+
