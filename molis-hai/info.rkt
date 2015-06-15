95d0d2bcb2a8bbdc0435c6c4c3a5eef907fe0334
John Clements <clements@racket-lang.org>
2015-06-15 16:18:10 -0700

    added scribble docs

--- molis-hai/info.rkt
+++ molis-hai/info.rkt
@@ -2,12 +2,8 @@
 
 (define name "Molis Hai")
 
-;; hastily copied from portaudio. leaving these here as templates...
-#;(define scribblings '(("portaudio.scrbl" () (tool))))
-#;(define version "2015-10-13-12:44")
-#;(define release-notes '((p "improved windows host api handling, "
-                           "bugfixes")))
 (define compile-omit-paths '("experimental"))
+(define scribblings '(("molis-hai.scrbl" () (tool))))
 
 (define raco-commands
   (list
